371c05fe9068389092402c9c2911d13a0811eb88
Ting-Hsuan Lin <tlin40@jhu.edu>
2017-08-03 00:37:02 -0400

    a draft

--- CH17/vector.cpp
+++ CH17/vector.cpp
@@ -1,50 +1,75 @@
 #include "vector.h"
 
-template<typename T> void vvector<T>::reserve(int newalloc){
-
-	if(newalloc<=space) return; // never decrease allocation
+template<typename T, typename A> T& vvector<T,A>::at(int n){
 	
-	// allocate new space
-	T* p = new T[newalloc];
-	// copy and delete old elements
-	for(int i=0; i<sz; ++i) p[i] = elem[i];
-	delete[] elem;
-	elem = p;
-	space = newalloc;
+	if(n<0 || this->sz<=n) throw my_out_of_range();
+	return this->elem[n];
 
 }
 
-template<typename T> void vvector<T>::resize(int newsize){
+template<typename T, typename A> const T& vvector<T,A>::at(int n) const{
+	
+	if(n<0 || this->sz<=n) throw my_out_of_range();
+	return this->elem[n];
+
+}
+
+template<typename T, typename A> void vvector<T,A>::reserve(int newalloc){
+
+	if(newalloc<=this->space) return; // never decrease allocation
+	
+	vvector_base<T,A> b(this->alloc,newalloc); // allocate new space
+	uninitialized_copy(this->elem,this->elem[this->sz],b.elem);// copy: uninitialized_copy can handle the throw
+
+	for(int i=0; i<this->sz; ++i)
+		this->alloc.destroy(&this->elem[i]); 
+	this->alloc.deallocate(this->elem,this->space);// deallocate old space
+		
+	swap<vvector_base<T,A>>(*this,b); // swap representations
+
+}
+
+template<typename T, typename A> void vvector<T,A>::resize(int newsize, T val){
+	// that is, use T() as the default value unless the user says otherwise
 
 	reserve(newsize);
-	for(int i=sz; i<newsize; ++i) elem[i] = 0;
-	sz = newsize;
+
+	for(int i=this->sz; i<newsize; ++i)
+		this->alloc.construct(&this->elem[i],val);
+
+	for(int i = newsize; i<this->sz; ++i)
+		this->alloc.destroy(&this->elem[i]);
+
+	this->sz = newsize;
 
 }
 
-template<typename T> void vvector<T>::push_back(T d){
+template<typename T, typename A> void vvector<T,A>::push_back(const T& val){
 
-	if(space==0)
+	if(this->space==0)
 		reserve(8); // start with space for 8 elements
-	else if(sz==space)
-		reserve(2*space); // get more space
+	else if(this->sz==this->space)
+		reserve(2*this->space); // get more space
 
-	elem[sz] = d; // add d at end
-	++sz;
+	this->alloc.construct(&this->elem[this->sz],val);
+	++this->sz;
 
 }
 
-vector<Shape> vs;
-vector<Circle> vc;
-vs = vc; // error: vector<Shape> required
+//---------------------------------------------------- helper function
 
-void f(vector<Shape>&);
-f(vc); // error: vector<Shape> required
+void print_some(vvector<int>& v){
 
-void f(vector<Shape&>){
-	v.push_back(new Rectangle(Point(0,0),Point(100,100)));
-	// how can we store Rectangle* inside vector<Circle*>?
+	int i = -1;
+	cin >> i;
+	while(i!=-1) try{
+		cout << "v[" << i << "]==" << v.at(i) << "\n";
+	}catch(my_out_of_range){
+		cout << "bad index: " << i << "\n";
+	}
+
 }
 
+int main(){
 
-
+}
--- CH17/vector.h
+++ CH17/vector.h
@@ -1,39 +1,55 @@
 #include "std_lib_facilities.h"
 
-template<typename T> // prefix
-class vvector{
+struct my_out_of_range{}; // class used to report range access errors
 
-	/* 
+template<typename T, typename A = allocator<T>> struct vvector_base{
+	// deal with memory rather than typed object
+	A alloc;
+	T* elem;
+	int sz;
+	int space;
+
+	vvector_base(const A& a, int n)
+		: alloc{a}, elem{a.allocate(n)}, sz{n}, space(n){}
+
+	~vvector_base(){
+		alloc.deallocate(elem,space);
+	}
+};
+
+template<typename T, typename A = allocator<T>> // prefix
+class vvector: private vvector_base<T,A>{
+
+	/*
+	A alloc; // use allocate to handle memory for elements
+
 		invariant:
 		1.	if 0<=n && n<sz, elem[n] is element n
 		2.	sz<=space
 		3.	sz<space there's space for (space-sz) doubles after elem[sz-1]
-	*/
 
 	int sz;
-	T* elem;
+	T* elem; // elem is a pointer of T
 	int space; // number of elements + "free space"/"slots"
+	*/
 
 public:
-
 	// default constructor
 	vvector()
-		:sz{0}, elem{nullptr}, space{0}{}
+		:vvector::sz{0}, vvector::elem{nullptr}, vvector::space{0}{}
 
 	// constructor
-	explicit vvector(int s) // allocates s doubles
-		:sz{s}, elem{new T[s]}, space{s}{
+	explicit vvector(int s, T val = T()) // allocates s Ts
+		:vvector::sz{s}, vvector::elem{vvector::alloc.allocate(s)}, vvector::space{s}{
 			// initialization
-			for(int i=0; i<s; ++i) elem[i] = 0; 
-	
+			for(int i=0; i<s; ++i) vvector::alloc.construct(&vvector::elem[i],val); 
 	}
 
 	// constructor (initializer_list)
 	vvector(initializer_list<T> lst)
-		: sz(lst.size()), elem{new T[sz]}, space{sz}{
+		: vvector::sz(lst.size()), vvector::elem{vvector::alloc.allocate(vvector::sz)}, vvector::space{vvector::sz}{
 			// initialization using std::copy B.5.2
-			copy(lst.begin(),lst.end(),elem); 
-	
+			copy(lst.begin(),lst.end(),vvector::elem); 
 	}
 
 	// copy constructor; initialization
@@ -50,91 +66,81 @@ public:
 
 	// destructor
 	~vvector(){ 
-		delete[] elem; // to free memory
+		vvector::alloc.deallocate(vvector::elem, vvector::space); // to free memory
 	}
-
-	//------------------------------------------------ functions
 
 	// using [] notation
-	T& operator[](int n){ // non-const version
-		return elem[n];
+	T& operator[](int n){ // non-const version / unchecked access
+		return vvector::elem[n];
 	}
 
-	T operator[](int n) const{ // const version
-		return elem[n];
+	T operator[](int n) const{ // const version / unchecked access
+		return vvector::elem[n];
 	};
 
-	/* abandoned
-	double get(int n){
-		return elem[n];
-	}
+	T& at(int n); // checked access
 
-	void set(int n, double v){
-		elem[n] = v;
-	}
-	*/
+	const T& at(int n) const; // checked access
 
-	int size() const { return sz; } // current size (elements are initialized)
+	int size() const { return vvector::sz; } // current size (elements are initialized)
 
-	int capacity() const{ return space; } // current space (elements are not initialized)
-
+	int capacity() const{ return vvector::space; } // current space (elements are not initialized)
 
 	void reserve(int newalloc); // add space for new elements
 
-	void resize(int newsize); // newsize (elements are initialized)
+	void resize(int newsize, T val=T()); // newsize (elements are initialized)
 	
-	void push_back(T d);
+	void push_back(const T& d);
 
 };
 
 
-template<typename T> vvector<T>::vvector(const vvector& arg) 
-	// allocate elements and initializa by std::copy
-	:sz{arg.sz}, elem{new T[arg.sz]}, space{sz}{
-		copy(arg.elem, arg.elem+arg.sz, elem); // B.5.2
+template<typename T, typename A> vvector<T,A>::vvector(const vvector& arg) 
+	// allocate elements and initialize by std::copy
+	:vvector::sz{arg.sz}, vvector::elem{vvector::alloc.allocate(arg.sz)}, vvector::space{vvector::sz}{
+		copy(arg.elem, arg.elem+arg.sz, vvector::elem); // B.5.2
 
 }
 
 
-template<typename T> vvector<T>& vvector<T>::operator=(const vvector& a){
+template<typename T, typename A> vvector<T,A>& vvector<T,A>::operator=(const vvector& a){
 
 	if(this==&a) return *this; // self-assignment, no work needed
 
-	if(a.sz<=space){ // enough space, no need for new allocation
+	if(a.sz<=this->space){ // enough space, no need for new allocation
 
-		for(int i=0; i<a.sz; ++i) elem[i] = a.elem[i];
-		sz = a.sz;
+		for(int i=0; i<a.sz; ++i) this->elem[i] = a.elem[i];
+		this->sz = a.sz;
 		return *this;
 
 	}
 
 	// make this vector a copy of a
-	T* p = new T[a.sz]; // allocate new space
-	// for(int i=0; i<a.sz; ++i) p[i] = a.elem[i];
+	T* p = this->alloc.allocate(a.sz); // allocate new space
 	copy(a.elem, a.elem+a.sz, p); // copy elements
 
-	delete[] elem; // elem of this vector
+	this->alloc.deallocate(this->elem, this->space);
 
-	elem = p;
-	space = sz = a.sz;
+	this->elem = p;
+	this->space = this->sz = a.sz;
 	return *this;
 
 }
 
-template<typename T> vvector<T>::vvector(vvector&& a)
-	:sz{a.sz}, elem{a.elem}, space{sz}{
+template<typename T, typename A> vvector<T,A>::vvector(vvector&& a)
+	:vvector::sz{a.sz}, vvector::elem{a.elem}, vvector::space{vvector::sz}{
 		
 		a.space = a.sz = 0; // clear
 		a.elem = nullptr; // clear
 
 }
 
-template<typename T> vvector<T>& vvector<T>::operator=(vvector&& a){ // move a to this vector
+template<typename T, typename A> vvector<T,A>& vvector<T,A>::operator=(vvector&& a){ // move a to this vector
 
-	delete[] elem; // clear
+	this->alloc.deallocate(this->elem, this->space);
 	
-	elem = a.elem;
-	space = sz = a.sz;
+	this->elem = a.elem;
+	this->space = this->sz = a.sz;
 
 	a.elem = nullptr; // clear
 	a.space = a.sz = 0; // clear 
@@ -143,3 +149,16 @@ template<typename T> vvector<T>& vvector<T>::operator=(vvector&& a){ // move a t
 
 }
 
+/* A simplified version of std allocator
+
+	template<typename T> class allocator{
+
+	public:
+		T* allocate(int n); // allocate space for n objects of type T
+		void deallocate(T* p, int n); // deallocate n objects of type T with pointer p
+
+		void construct(T* p, const T& v); // construct a T with the value v with pointer p
+		void destroy(T* p); // destroy the T with pointer p
+
+	}
+*/
